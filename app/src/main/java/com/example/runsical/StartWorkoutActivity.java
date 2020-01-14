aa85cef91321105ab74d758e3cbce63976707972
maingo <mtngo@smith.edu>
2020-01-14 14:44:41 -0500

    Merge branch 'master' of https://github.com/asmitagautam/Runsical
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/runsical/StartWorkoutActivity.java
+++ app/src/main/java/com/example/runsical/StartWorkoutActivity.java
@@ -2,7 +2,11 @@ package com.example.runsical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+
+import java.io.IOException;
 
 public class StartWorkoutActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class StartWorkoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_workout);
+    }
+
+    private void playMusic() {
+        String url = "http://........"; // your URL here
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepareAsync(); // might take long! (buffering, fetching and decoding media data)
+        } catch (IOException e) {
+            // TODO: display error message in app
+        }
+        mediaPlayer.start();
     }
 }
