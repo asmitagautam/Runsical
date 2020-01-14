369049283e13c3ede32ca4351b573266d511ff6b
demo <demo@demos-MacBook-Pro.local>
2020-01-14 14:37:19 -0500

    sign up page UI

--- app/src/main/java/com/example/runsical/MainActivity.java
+++ app/src/main/java/com/example/runsical/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.runsical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting app name textView to red
+        TextView appName = findViewById(R.id.runsical);
+        appName.setTextColor(getResources().getColor(R.color.red));
+
+        //setting create account textView to red
+        TextView createAccount = findViewById(R.id.createaccount);
+        createAccount.setTextColor(getResources().getColor(R.color.red));
+
+        //Click listener for create new account activity
+        createAccount.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(MainActivity.this, CreateNewAccount.class);
+                                           startActivity(intent);
+                                       }
+                                   }
+        );
+
     }
+
 }
